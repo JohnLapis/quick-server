873371564fb3bb88e9d521cd63bf3f3e7ca712c1
john <borges-2014@live.com>
2020-10-10 20:34:50 -0300

    style: black applied

--- index.py
+++ index.py
@@ -4,21 +4,20 @@ import markdown, subprocess
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
     poems = []
     all_poems = subprocess.run(
-        ["node", "poem-scraper.js"],
-        stdout=subprocess.PIPE
+        ["node", "poem-scraper.js"], stdout=subprocess.PIPE
     ).stdout.decode()
     for poem in all_poems.split("<ENDPOEM>"):
         poem_lines = poem.strip().split("\n")
-        poems.append({
-            "title": poem_lines[0],
-            "text": poem_lines[2:],
-        })
+        poems.append(
+            {
+                "title": poem_lines[0],
+                "text": poem_lines[2:],
+            }
+        )
 
-    return render_template(
-        "base.html",
-        poems=poems
-    )
+    return render_template("base.html", poems=poems)
