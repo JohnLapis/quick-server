13d61b9ca4619ba189b865d38b6db882d59a7235
john <borges-2014@live.com>
2020-10-13 11:13:10 -0300

    update index to get numbers instead of poems

--- index.py
+++ index.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-import subprocess
+from scrapers import math_scraper
 
 
 app = Flask(__name__)
@@ -7,20 +7,13 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    poems = []
-    all_poems = subprocess.run(
-        ["node", "poem-scraper.js"], stdout=subprocess.PIPE
-    ).stdout.decode()
-    for poem in all_poems.split("====="):
-        poem_lines = poem.strip().split("\n")
-        poems.append(
-            {
-                "title": poem_lines[0],
-                "text": poem_lines[2:],
-            }
-        )
+    fact = math_scraper.main()
+    return render_template("base.html", fact=fact)
 
-    return render_template("base.html", poems=poems)
+@app.route("/<int:number>")
+def number(number):
+    fact = math_scraper.main(number)
+    return render_template("base.html", fact=fact)
 
 
 @app.route("/<name>")
